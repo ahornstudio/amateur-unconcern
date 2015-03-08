c407bb72b2dacd62a8394ebbf45b11e20ac86c26
Christian Nolle <christian@goodcaesar.com>
2015-03-08 13:34:19 +0000

    Gettings thing up on git

---
+++ js/site.js
@@ -0,0 +1,32 @@
+$(document).ready(function() { 
+	sizeContent();
+	centerVertical();
+	
+	$("#warning").backstretch("/photos/act2-part6-a.jpg");
+	
+});
+
+//Every resize of window
+$(window).resize(function() {
+	sizeContent;
+	centerVertical();
+});
+
+//Dynamically assign height
+function sizeContent() {
+    var newHeight = $("html").height() + "px";
+    $("section.full-height").css("height", newHeight);
+}
+
+function centerVertical() {
+	
+	var halfitem = $('.vertical-center').height() / 2;
+	
+	var newmargin = $("html").height() / 2 - halfitem + "px";
+	//var topmargin = fullheight - $('.vertical-center').height() + 'px';	
+	
+	console.log(halfitem);
+	console.log(newmargin);
+	
+    $(".vertical-center").css("margin-top",newmargin);
+}
