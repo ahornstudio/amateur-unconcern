8d48ef35483620deb6684bd598e68bef74d23ed2
Christian Nolle <christian@goodcaesar.com>
2015-03-09 15:45:55 +0000

    commit monday 9th

--- js/site.js
+++ js/site.js
@@ -6,6 +6,15 @@ $(document).ready(function() {
 	$("#act4-full").backstretch("/photos/act4-part1.jpg");	
 	
 	
+//	$('picture.hascaption').captionjs({
+//	       'class_name'      : 'captionjs', // Class name for each <figure>
+//	       'schema'          : true,        // Use schema.org markup (i.e., itemtype, itemprop)
+//	       'mode'            : 'default',   // default | stacked | animated | hide
+//	       'debug_mode'      : false,       // Output debug info to the JS console
+//	       'force_dimensions': true,        // Force the dimensions in case they cannot be detected (e.g., image is not yet painted to viewport)
+//	       'is_responsive'   : true,       // Ensure the figure and image change size when in responsive layout. Requires a container to control responsiveness!
+//	       'inherit_styles'  : false        // Have the caption.js container inherit box-model properties from the original image
+//	   });	
 });
 
 //Every resize of window
@@ -16,8 +25,11 @@ $(window).resize(function() {
 
 //Dynamically assign height
 function sizeContent() {
-    var newHeight = $("html").height() + "px";
-    $("section.full-height").css("height", newHeight);
+    //var newHeight = $("html").height() + "px";
+	
+	var newHeight = $(window).height();
+	$("section.full-height").css("height", newHeight);
+
 }
 
 function centerVertical() {
